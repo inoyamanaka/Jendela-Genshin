c14b7f9a79b66c664f0b8a01c5a5f62fedc4b3ae
Lambang <gilbertrafdi@gmail.com>
2023-11-24 06:29:53 +0700

    tess

--- lib/features/characters/presentation/widgets/character_card.dart
+++ lib/features/characters/presentation/widgets/character_card.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:genshin_app/features/characters/presentation/pages/detail_character_page.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -29,18 +30,18 @@ class CharacterCard extends StatelessWidget {
           Container(
             height: 200,
             width: 200,
-            decoration: BoxDecoration(
+            clipBehavior: Clip.hardEdge,
+            decoration: const BoxDecoration(
               // color: Colors.grey,
-              image: DecorationImage(
-                image: NetworkImage(
-                  background,
-                ),
-                fit: BoxFit.cover, // Atur sesuai kebutuhan Anda
-              ),
-              borderRadius: const BorderRadius.only(
+
+              borderRadius: BorderRadius.only(
                 topLeft: Radius.circular(45),
                 bottomRight: Radius.circular(45),
               ),
+            ),
+            child: CachedNetworkImage(
+              fit: BoxFit.cover,
+              imageUrl: background,
             ),
           ),
           Positioned(
@@ -48,26 +49,8 @@ class CharacterCard extends StatelessWidget {
             bottom: 5,
             left: 25,
             right: 5,
-            child: Image.network(
-              imgChar,
-              scale: 0.3,
-              errorBuilder:
-                  (BuildContext context, Object error, StackTrace? stackTrace) {
-                // Menampilkan gambar alternatif ketika terjadi kesalahan
-                if (error is NetworkImageLoadException &&
-                    error.statusCode == 404) {
-                  // Gambar tidak ditemukan, tampilkan gambar alternatif
-                  return Image.network(
-                    'https://static.vecteezy.com/system/resources/previews/024/658/986/non_2x/3d-male-character-holding-and-presenting-to-a-laptop-with-empty-screen-free-png.png',
-                    scale: 0.3,
-                  );
-                } else {
-                  return Image.network(
-                    'https://static.vecteezy.com/system/resources/previews/024/658/986/non_2x/3d-male-character-holding-and-presenting-to-a-laptop-with-empty-screen-free-png.png',
-                    scale: 0.3,
-                  );
-                }
-              },
+            child: CachedNetworkImage(
+              imageUrl: imgChar,
             ),
           ),
           Positioned(
